683b1f153a8c2753f637a46f1492c48e88a449bd
imseoyeong <67615331+imseoyeong@users.noreply.github.com>
2020-08-25 10:35:41 +0900

    d

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(function(){
+    $(".box").each(function () {
+     $(this).click(function () {
+       var img_name = $(this).find("a");
+       var img_src = $(img_name).attr("phref");
+       var img_title = $(img_name).attr("title");
+       var img_size = $(img_name).attr("data");
+       $(".pop").show();
+       $(".pop img").attr("src", img_src);
+       $(".pop .txt").text( img_title);
+       $(".pop img").width(img_size);
+     });
+
+     $('.close, .pop').click(function(){
+       $(".pop").hide();
+       })
+   });
+
+    /*   $('.graphic_box a').each(function(){
+      $('.pop').hide();
+      $(this).click(function(){
+        var ahref = $(this).attr('href');
+        $('.pop').hide();
+        $(ahref).fadeIn();
+        $('.graphic_box a').css({background:'none'});
+        $(this).css({'background-color':'orange'});
+
+        $('.close').click(function(){
+          $(ahref).fadeOut(500);
+            })
+
+
+       
+      })
+  })*/
+
+
+   });
